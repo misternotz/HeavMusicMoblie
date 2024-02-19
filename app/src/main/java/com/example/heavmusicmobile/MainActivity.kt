48f9856de0a4ea797fe229083d689f7e7a9d7eba
misternotz <rilakuma129@hotmail.com>
2024-02-20 00:00:14 +0700

    Finished3

--- app/src/main/java/com/example/heavmusicmobile/MainActivity.kt
+++ app/src/main/java/com/example/heavmusicmobile/MainActivity.kt
@@ -41,6 +41,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     HeavMusicMobileTheme {
-        Greeting("Hello World")
+        Greeting("Hello Worldv 2")
     }
 }
